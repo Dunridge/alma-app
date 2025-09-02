4a0a216c17c242f563fe45990e8ffa34903d6b10
Dunridge <Polinka7max@gmail.com>
2025-09-02 13:50:29 -0700

    add fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 "use client";
+
 import ImmigrationAssessmentForm from "@/components/ImmigrationAssessmentForm";
+import logo from "@/assets/svg/logo.svg";
+import Image from "next/image";
 
 export default function Home() {
   // login page
   return (
-    <div>
+    <div className="home">
+      <div className="home__header">
+        <div className="home__header-container">
+          <Image src={logo} alt="Logo" width={50} height={50} />
+          <div className="home__header-title">
+            <span>Get An Assessment</span>
+            <span>Of Your Immigration Claim</span>
+          </div>
+        </div>
+      </div>
       <ImmigrationAssessmentForm />
     </div>
   );
