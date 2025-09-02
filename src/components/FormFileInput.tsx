1fb123c5b10171b33af2c5f0d47d0dfb20877a23
Dunridge <Polinka7max@gmail.com>
2025-09-02 16:34:44 -0700

    style input

--- src/components/FormFileInput.tsx
+++ src/components/FormFileInput.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 type Props = {
   label: string;
   file: File | null;
@@ -7,6 +9,26 @@ type Props = {
 };
 
 export default function FormFileInput({ label, file, onChange }: Props) {
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDragging(false);
+    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+      onChange(e.dataTransfer.files[0]);
+      e.dataTransfer.clearData();
+    }
+  };
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
       onChange(e.target.files[0]);
@@ -16,8 +38,20 @@ export default function FormFileInput({ label, file, onChange }: Props) {
   };
 
   return (
-    <div className="immigration-form__group immigration-form__group--file-upload">
+    <div
+      className={`immigration-form__group immigration-form__group--file-upload ${
+        isDragging ? "drag-hover" : ""
+      }`}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+    >
       <label className="immigration-form__group-title">{label}</label>
+      {!file && (
+        <div className="immigration-form__group--file-upload-placeholder">
+          Drag&Drop your file
+        </div>
+      )}
       <input
         type="file"
         accept=".pdf,.doc,.docx"
