5ad6a60147dd3a23ef0a3ab5b6f538e83d883efe
Dunridge <Polinka7max@gmail.com>
2025-09-03 09:09:39 -0700

    define styles

--- src/app/menu/settings/page.tsx
+++ src/app/menu/settings/page.tsx
@@ -1,8 +1,9 @@
 export default function Settings() {
+  // TODO: define the settings page
   return (
     <div className="settings">
       <div className="settings__title">Settings</div>
-      <div className="">NA</div>
+      <div className="">No settings, yet: check in as the app grows...</div>
     </div>
   );
 }
