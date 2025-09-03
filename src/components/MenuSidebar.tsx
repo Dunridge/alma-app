fe08e925254000fd8f4e54b94b8b5675fc740d78
Dunridge <Polinka7max@gmail.com>
2025-09-02 18:26:34 -0700

    add sidebar options selection

--- src/components/MenuSidebar.tsx
+++ src/components/MenuSidebar.tsx
@@ -1,18 +1,19 @@
-import ImmigrationAssessmentForm from "@/components/ImmigrationAssessmentForm";
 import logo from "@/assets/svg/logo.svg";
 import Image from "next/image";
-import Link from "next/link";
+import SidebarOption from "./SidebarOption";
 
 type Props = {};
-
-const SidebarOption = ({ href, text }: { href: string; text: string }) => {
-  return <Link href={href}>{text}</Link>;
-};
 
 export default function MenuSidebar({}: Props) {
   return (
     <div className="menu-sidebar">
-      <Image src={logo} alt="Logo" width={50} height={50} />
+      <Image
+        className="menu-sidebar__logo"
+        src={logo}
+        alt="Logo"
+        width={100}
+        height={60}
+      />
 
       <div className="menu-sidebar__options">
         <SidebarOption href="/menu/leads" text="Leads" />
